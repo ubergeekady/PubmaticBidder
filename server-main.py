ee26dc671df5b23c89db115cd0df48fc07826430
Aditya Singh <aditya@impulsemedia.co.in>
2013-10-15 08:00:40 +0530

    modified:   server-main.py

--- server-main.py
+++ server-main.py
@@ -124,6 +124,17 @@ class MainHandler(tornado.web.RequestHandler):
 	
         timeTaken = time.time() - start
 
+	bidCountIndex["Pubmatic"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"] += 1
+	if int(time.time() - bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"])>120:
+	  i = bidCountIndex["Pubmatic"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]
+	  message = json.dumps({"messageType":"Forecast", "message":{"e":"GoogleAdX", "d":domain , "c":"DesktopDisplay" ,"geo":country.upper(),
+				  "size":str(ad.width[0])+'x'+str(ad.height[0]) , "i":i}})
+	  bidCountIndex["Pubmatic"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]=0
+	  bidCountIndex["Pubmatic"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"]=time.time()
+	  sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	  sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	  sock.sendto(message, (UDP_IP, UDP_PORT))        
+
 def autovivify(levels=1, final=dict):
     return (defaultdict(final) if levels < 2 else defaultdict(lambda: autovivify(levels - 1, final)))
 
